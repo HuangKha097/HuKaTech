6a3cd2709cf0b57f4f43c48f5bff8dcd1b560cbf
HuangKha097 <huangkha097@gmail.com>
2026-03-13 13:02:13 +0700

    fix cors for netlify

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,20 +11,25 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 // Cấu hình CORS
-const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001'];
+const allowedOrigins = [
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "https://hukatech.netlify.app"
+];
+
 app.use(cors({
     origin: function (origin, callback) {
         if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            const msg = 'Lỗi CORS: Origin này không được phép truy cập.';
-            return callback(new Error(msg), false);
+
+        if (!allowedOrigins.includes(origin)) {
+            return callback(new Error("CORS blocked"), false);
         }
+
         return callback(null, true);
     },
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
+    credentials: true
 }));
-
 app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({limit: "50mb", extended: true}));
 
